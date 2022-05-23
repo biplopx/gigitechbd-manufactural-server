9a681519a5e2900714e2f751ffcb96e2fed1f928
MD Biplop Hossain <developerbiplop@gmail.com>
2022-05-23 14:48:10 +0600

    Product details api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -22,12 +22,23 @@ async function run() {
     // dabase name collection
     const productsCollection = client.db('gigitechbd').collection('products');
 
+    /* All prouct api */
     app.get('/products', async (req, res) => {
       const query = {};
       const cursor = productsCollection.find(query);
       const products = await cursor.toArray();
       res.send(products);
     })
+
+    // Product Details api
+    app.get('/product/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const product = await productsCollection.findOne(query);
+      res.send(product);
+    })
+
+
 
   }
   finally {
